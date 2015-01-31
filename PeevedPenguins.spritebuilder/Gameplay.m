157a04d228b7d2f02825b0b2d92dcca91372bb93
EDFward <edfward@gmail.com>
2015-01-30 22:33:02 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -50,7 +50,7 @@
                               damping:150.f];
 
     _currentPenguin = [CCBReader load:@"Penguin"];
-    CGPoint penguinPosition = [_catapultArm convertToWorldSpace:ccp(34, 183)];
+    CGPoint penguinPosition = [_catapultArm convertToWorldSpace:ccp(34, 160)];
     _currentPenguin.position =
         [_physicsNode convertToNodeSpace:penguinPosition];
 
