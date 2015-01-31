403f072f27429278b05aeae02f14f1f703c1df0d
EDFward <edfward@gmail.com>
2015-01-30 22:05:37 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -14,7 +14,7 @@
   CCNode *_levelNode;
   CCNode *_contentNode;
   CCNode *_pullbackNode;
-  
+
   CCNode *_mouseJointNode;
   CCPhysicsJoint *_mouseJoint;
 }
@@ -23,17 +23,49 @@
   self.userInteractionEnabled = TRUE;
   CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
   [_levelNode addChild:level];
-  
+
   // for DEBUG
   _physicsNode.debugDraw = TRUE;
-  
+
   _pullbackNode.physicsBody.collisionMask = @[];
-  
+
   _mouseJointNode.physicsBody.collisionMask = @[];
 }
 
 - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
-  [self launchPenguin];
+  CGPoint touchLocation = [touch locationInNode:_contentNode];
+
+  if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation)) {
+    _mouseJointNode.position = touchLocation;
+    _mouseJoint = [CCPhysicsJoint
+        connectedSpringJointWithBodyA:_mouseJointNode.physicsBody
+                                bodyB:_catapultArm.physicsBody
+                              anchorA:ccp(0, 0)
+                              anchorB:ccp(34, 138)
+                           restLength:0.f
+                            stiffness:3000.f
+                              damping:150.f];
+  }
+}
+
+- (void)touchMoved:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+  CGPoint touchLocation = [touch locationInNode: _contentNode];
+  _mouseJointNode.position = touchLocation;
+}
+
+- (void)touchEnded:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+  [self releaseCatapult];
+}
+
+- (void)touchCancelled:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+  [self releaseCatapult];
+}
+
+- (void) releaseCatapult {
+  if (_mouseJoint != nil) {
+    [_mouseJoint invalidate];
+    _mouseJoint = NULL;
+  }
 }
 
 - (void)launchPenguin {
